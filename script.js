bead2923d68b91b8b4f34cefd1eda4875d85b454
Islom <islom@gmail.com>
2022-11-19 17:55:28 +0300

    islom

---
+++ script.js
@@ -0,0 +1,64 @@
+// tekshiruvchi operatorlar true yoki false to'g'ri yoki noto'g'ri javob qaytaradi
+
+//  var a=2
+//   console.log(a==2)
+
+//  var misol = +prompt("6+3=?")
+//  console.log("Sizning javob to'g'ri: "+ misol)
+
+
+
+
+// console.log("6+3="+(6+3));
+// console.log("6-3="+(6-3));
+// console.log("6*3="+6*3);
+// console.log("6/3="+6/3);
+// console.log("13%3="+13%3);
+
+var ism = prompt ("Ismingiz nima ?")
+console.log ("Mening ismim: " + ism);
+
+
+var age = +prompt("Yoshingiz nechida ?")
+console.log ("Mening yoshim: " + age)
+
+
+var misol = +prompt ("6+3=?")
+if  (misol == 9) {
+    alert("Sizning javob to'g'ri: " +misol )
+} else {
+    alert("Sizning javob noto'g'ri: " +misol );
+}
+
+
+var misol = +prompt ("6-3=?")
+if  (misol == 3) {
+    alert("Sizning javob to'g'ri: " +misol )
+} else {
+    alert("Sizning javob noto'g'ri: " +misol );
+}
+
+
+var misol = +prompt ("6*2=?")
+if  (misol == 12) {
+    alert("Sizning javob to'g'ri: " +misol )
+} else {
+    alert("Sizning javob noto'g'ri: " +misol );
+}
+
+
+var misol = +prompt ("6/3=?")
+if  (misol == 2) {
+    alert("Sizning javob to'g'ri: " +misol )
+} else {
+    alert("Sizning javob noto'g'ri: " +misol );
+}
+
+
+var misol = +prompt ("27%2=?")
+if  (misol == 1) {
+    alert("Sizning javob to'g'ri: " +misol )
+} else {
+    alert("Sizning javob noto'g'ri: " +misol );
+}
+
